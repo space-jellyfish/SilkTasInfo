866cfdaecbc34c21cad6c5ae9338afba54dc74d8
Xiangyu Chen <chenxy327@gmail.com>
2026-08-10 13:49:25 -0600

    fix cameraZoom

--- Patches/CameraController.cs
+++ Patches/CameraController.cs
@@ -1,17 +1,22 @@
 using Assembly_CSharp.TasInfo.mm.Source;
 using UnityEngine;
+using System.Collections;
 
 // ReSharper disable All
 class patch_CameraController : CameraController {
     private void Awake() {
         Application.onBeforeRender += OnApplicationBeforeRender;
+        StartCoroutine(OnWaitForEndOfFrame());
     }
     
     private void OnApplicationBeforeRender() {
         TasInfo.OnPreRender();
     }
 
-    private void OnPostRender() {
-        TasInfo.OnPostRender();
+    private IEnumerator OnWaitForEndOfFrame() {
+        while (true) {
+            yield return new WaitForEndOfFrame();
+            TasInfo.OnPostRender();
+        }
     }
 }
